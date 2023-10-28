4e436325811f05a03ac6264eb698f935a63758f4
Fabio Sbano <fsbano@yahoo.com>
2023-10-28 12:23:26 -0300

    modified:   spotlight.js 	modified:   spotlight.yaml

--- spotlight.js
+++ spotlight.js
@@ -6,18 +6,14 @@ const app = express();
 
 try { 
   const config = yaml.load(fs.readFileSync('./spotlight.yaml'))
-  for ( const integrations of config.integrations ) {
-    for (const isEnabled of Object.values(integrations) ) {
-      if ( isEnabled == true ) {
-        const extensions = require('./plugins/' + Object.keys(integrations));
-        if (typeof extensions.execute === 'function') {
-          app.get(`/api/v1/${extensions.name.toLowerCase().replace(/\s+/g, '-')}`, (req, res) => {
-            res.send(extensions.execute());
-          });
-        }  
-      }  
-    }
-  }
+  Object.keys(config.integrations).map(function(name) {
+    const extensions = require('./plugins/' + name );
+    if (typeof extensions.execute === 'function') {
+      app.get(`/api/v1/${extensions.name.toLowerCase().replace(/\s+/g, '-')}`, (req, res) => {
+        res.send(extensions.execute());
+      });
+    }  
+  });
   const PORT = 3000;
   app.listen(PORT, () => {
     console.log('Spotlight Version 1.0.0');
